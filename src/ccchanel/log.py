8aa0112ae1055d8620445f705e61d647757d1e16
NegaScout <jenik.wagner@gmail.com>
2024-01-03 14:53:42 +0100

    log wrappers

--- src/ccchanel/log.py
+++ src/ccchanel/log.py
@@ -1,21 +1,23 @@
 import json
-
-def log_append(path, entry):
-    log = log_read(path)
+import file
+def log_append(dbx, path, entry):
+    log = log_read(dbx, path)
     log += [entry]
-    with open(path, 'w') as file_handle:
-        json.dump(log, file_handle)
-
-def log_read(path):
-    with open(path, 'r') as file_handle:
-        return json.load(file_handle)
-
-def log_create(path):
-    with open(path, 'w') as file_handle:
-        json.dump([], file_handle)
+    payload = file.encode_to_bytes(log)
+    file.upload_file_fp(dbx, payload, path)
+def log_read(dbx, path):
+    bytes = file.download_file(dbx, path)
+    return file.decode_from_bytes(bytes)
+def log_init(dbx, path):
+    payload = file.encode_to_bytes([])
+    file.upload_file_fp(dbx, payload, path)
 
 if __name__ == '__main__':
-    log_create('resources/log')
-    print(log_read('resources/log'))
-    log_append('resources/log', 'Hello World!')
-    print(log_read('resources/log'))
+    import dropbox_api
+    dbx = dropbox_api.dropbox_login()
+    log_init(dbx, '/log.txt')
+    print(log_read(dbx, '/log.txt'))
+    log_append(dbx, '/log.txt', 'Hello World!')
+    print(log_read(dbx, '/log.txt'))
+    log_append(dbx, '/log.txt', 'Hello World! For the second time')
+    print(log_read(dbx, '/log.txt'))
