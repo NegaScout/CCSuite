dc91400257fe59e7eb4bbd02c3ffe0d49f069fe2
NegaScout <jenik.wagner@gmail.com>
2024-01-03 13:15:35 +0100

    encode, decode, upload from fp

--- src/ccchanel/file.py
+++ src/ccchanel/file.py
@@ -1,4 +1,5 @@
-import dropbox
+import dropbox, json
+from io import BytesIO
 def list_files(dbx, path, *args):
     # stolen and modified from https://github.com/dropbox/dropbox-sdk-python/blob/main/example/updown.py
     try:
@@ -12,17 +13,20 @@ def list_files(dbx, path, *args):
             rv[entry.name] = entry
         return rv
 
-def upload_file(dbx, path, name, *args):
+def upload_file_fp(dbx, file_handle, name, *args):
     # stolen and modified from https://github.com/dropbox/dropbox-sdk-python/blob/main/example/updown.py
     mode = dropbox.files.WriteMode.overwrite
-    with open(path, 'rb') as file_handle:
-        data = file_handle.read()
+    data = file_handle.read()
     try:
         res = dbx.files_upload(data, name, mode, mute=True)
     except dropbox.exceptions.ApiError as err:
         print('*** API error', err)
         return None
     return res
+def upload_file(dbx, path, name, *args):
+    # stolen and modified from https://github.com/dropbox/dropbox-sdk-python/blob/main/example/updown.py
+    with open(path, 'rb') as file_handle:
+        upload_file_fp(dbx, file_handle, name)
 
 def download_file(dbx, path, *args):
     # stolen and modified from https://github.com/dropbox/dropbox-sdk-python/blob/main/example/updown.py
@@ -38,14 +42,16 @@ def download_file_as(dbx, path_remote, path_local, *args):
     with open(path_local, 'wb') as file_handle:
         file_handle.write(download_file(dbx, path_remote))
 
-def encode_file(path, output_path, payload, *args):
-    pass
-def decode_file(path, *args):
-    pass
+def encode_to_bytes(payload: object, *args):
+    return BytesIO(json.dumps(payload).encode())
+def decode_from_bytes(bytes, *args):
+    return json.loads(bytes.decode())
 
 if __name__ == '__main__':
     import dropbox_api
     dbx = dropbox_api.dropbox_login()
     print(list_files(dbx, ''))
-    #upload_file(dbx, "/home/honza/Downloads/niceu.jpg", '/nice.jpg')
-    download_file_as(dbx, '/nice.jpg', '/home/honza/Downloads/downloaded')
+    upload_file_fp(dbx, encode_to_bytes(['payload']), '/payloads.txt')
+    #downloaded_file = download_file(dbx, '/payload.txt')
+    #print(type(decode_file(downloaded_file)))
+    #download_file_as(dbx, '/nice.jpg', '/home/honza/Downloads/downloaded')
