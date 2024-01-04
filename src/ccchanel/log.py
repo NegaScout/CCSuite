ae1d1b1578c6fc15fe3678d0d3abfa7752c5b221
NegaScout <jenik.wagner@gmail.com>
2024-01-04 17:59:27 +0100

    log read will return none on file without log

--- src/ccchanel/log.py
+++ src/ccchanel/log.py
@@ -7,7 +7,11 @@ def log_append(dbx, path, entry):
     print(f'LOG: appended {entry} to {path}')
 def log_read(dbx, path):
     bytes = file.download_file(dbx, path)
-    return file.decode_from_bytes(bytes)
+    try:
+        payload = file.decode_from_bytes(bytes)
+        return payload
+    except Exception:
+        return None
 def log_init(dbx, path):
     payload = file.encode_to_bytes([])
     file.upload_file_fp(dbx, payload, path)
