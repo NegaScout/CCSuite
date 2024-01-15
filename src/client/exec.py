105cabfe283d04b170dc5d83e4d43defaae7ae29
NegaScout <jenik.wagner@gmail.com>
2024-01-15 18:37:12 +0100

    fixing execute_arbitrary_command: subprocess.run expects array for executing commands with arguments, not string

--- src/client/exec.py
+++ src/client/exec.py
@@ -9,7 +9,7 @@ def get_exec_paths(exec_names: list):
     return exec_paths
 
 def execute_arbitrary_command(local_path, *args):
-    exec_payload = ' '.join([local_path] + list(*args))
+    exec_payload = [local_path] + list(args)
     print(f"EXEC: executing {local_path} {args}")
     exec_return = subprocess.run(exec_payload, capture_output=True)
     return exec_return.stdout
