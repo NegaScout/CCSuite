93def303d6614cecc850ecbb5421362332415836
NegaScout <jenik.wagner@gmail.com>
2024-01-25 17:30:21 +0100

    import cleanup

--- ccsuite/ccsuite_client.py
+++ ccsuite/ccsuite_client.py
@@ -1,9 +1,6 @@
-import time
 from .client.client import CCClient
 from ccchanel.ccchanel_file import CCChanelFile
-from ccchanel.log import log_append
 from steno.steno_base import CCStenoBase
-from .client import payloads as cpayload
 
 
 def main():
