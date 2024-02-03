97366aa0ac2e01ed2f5ac249013e8fc483ab5cbc
NegaScout <jenik.wagner@gmail.com>
2024-02-03 23:11:40 +0100

    cleanup

--- tests/dropbox_login_base.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-import dropbox
-import os
-
-
-class WithDropboxLogin(TestCase):
-    def setUp(self):
-        self.dbx = dropbox.Dropbox(os.environ['DROPBOX_TOKEN'])
